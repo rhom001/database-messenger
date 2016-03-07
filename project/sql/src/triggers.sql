13b64609477897297f52e9e2129c4142023b8832
Rebecca Hom <rhom001@well.cs.ucr.edu>
2016-03-07 13:42:42 -0800

    added in triggers file for use later

---
+++ project/sql/src/triggers.sql
@@ -0,0 +1,8 @@
+-- Triggers for database-messenger
+-- Trigger and procedure for putting in USER_LIST list_id
+
+-- Trigger and procedure for CHAT chat_id
+
+-- Trigger and procedure for MESSAGE msg_id and msg_timestamp
+
+
