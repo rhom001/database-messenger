4c6cc55103393b2411b5dc36b7f55ac097740d30
Rebecca Hom <rhom001@well.cs.ucr.edu>
2016-03-07 13:41:00 -0800

    modified data loading

--- project/sql/src/load_data.sql
+++ project/sql/src/load_data.sql
@@ -1,23 +1,24 @@
+-- For use on well
 COPY USER_LIST
-FROM 'project/data/usr_list.csv'
+FROM '/class/classes/rhom001/database-messenger/project/data/usr_list.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE user_list_list_id_seq RESTART 55906;
 
 COPY USR
-FROM '~/database-messenger/project/data/usr.csv'
+FROM '/class/classes/rhom001/database-messenger/project/data/usr.csv'
 WITH DELIMITER ';';
 
 COPY USER_LIST_CONTAINS
-FROM '~/database-messenger/project/data/usr_list_contains.csv'
+FROM '/class/classes/rhom001/database-messenger/project/data/usr_list_contains.csv'
 WITH DELIMITER ';';
 
 COPY CHAT
-FROM '~/database-messenger/project/data/chat.csv'
+FROM '/class/classes/rhom001/database-messenger/project/data/chat.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE chat_chat_id_seq RESTART 5001;
 
 COPY CHAT_LIST
-FROM '~/database-messenger/project/data/chat_list.csv'
+FROM '/class/classes/rhom001/database-messenger/project/data/chat_list.csv'
 WITH DELIMITER ';';
 
 COPY MESSAGE
@@ -26,7 +27,7 @@ COPY MESSAGE
 	msg_timestamp, 
 	sender_login,
 	chat_id)
-FROM '~/database-messenger/project/data/message.csv'
+FROM '/class/classes/rhom001/database-messenger/project/data/message.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE message_msg_id_seq RESTART 50000;
 
